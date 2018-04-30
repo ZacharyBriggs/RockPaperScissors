36aae492b2f3877e45496b86b5996135a927d267
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-04-30 14:19:18 -0500

    initial commit

---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game = new RPS();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox1.Text += "Player Chose: " + game.SelectPlayerChoice("rock") + "\n";
+            richTextBox1.Text += "Computer Chose: " + game.SelectComputerChoice() + "\n";
+            richTextBox1.Text += game.CheckForVictory();
+            string json = JsonConvert.SerializeObject(game.VictoryConditions);
+            var path = System.IO.Path.Combine(Environment.CurrentDirectory, "json.txt");
+            System.IO.File.WriteAllText(path, json);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox1.Text += "Player Chose: " + game.SelectPlayerChoice("paper") + "\n";
+            richTextBox1.Text += "Computer Chose: " + game.SelectComputerChoice() + "\n";
+            richTextBox1.Text += game.CheckForVictory();
+            string json = JsonConvert.SerializeObject(game.VictoryConditions);
+            var path = System.IO.Path.Combine(Environment.CurrentDirectory, "json.txt");
+            System.IO.File.WriteAllText(path, json);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox1.Text += "Player Chose: " + game.SelectPlayerChoice("scissors") + "\n";
+            richTextBox1.Text += "Computer Chose: " + game.SelectComputerChoice() + "\n";
+            richTextBox1.Text += game.CheckForVictory();
+            string json = JsonConvert.SerializeObject(game.VictoryConditions);
+            var path = System.IO.Path.Combine(Environment.CurrentDirectory, "json.txt");
+            System.IO.File.WriteAllText(path, json);
+        }
+    }
+}
---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public class RPS
+    {
+        public string PlayerChoice;
+        public string ComputerChoice;
+        public List<string> VictoryConditions = new List<string>();
+        public string SelectPlayerChoice(string word)
+        {
+            if (word == "rock")
+                PlayerChoice = "rock";
+            if (word == "paper")
+                PlayerChoice = "paper";
+            if (word == "scissors")
+                PlayerChoice = "scissors";
+            return word;
+        }
+
+        public string SelectComputerChoice()
+        {
+            Random rand = new Random();
+            var choice = rand.Next(1, 4);
+            if (choice == 1)
+                ComputerChoice = "rock";
+            if (choice == 2)
+                ComputerChoice = "paper";
+            if (choice == 3)
+                ComputerChoice = "scissors";
+            return ComputerChoice;
+        }
+
+        public string CheckForVictory()
+        {
+            string outcome = "";
+            if (PlayerChoice == "rock" && ComputerChoice == "paper" ||
+                PlayerChoice == "scissors" && ComputerChoice == "rock" ||
+                PlayerChoice == "paper" && ComputerChoice == "scissors")
+            {
+                outcome = "Computer Wins.\n";
+            }
+            else if (ComputerChoice == "rock" && PlayerChoice == "paper" ||
+                ComputerChoice == "scissors" && PlayerChoice == "rock" ||
+                ComputerChoice == "paper" && PlayerChoice == "scissors")
+            {
+                outcome = "Player Wins.\n";
+            }
+            else
+            {
+                outcome = "Nobody Wins.\n";
+            }
+            VictoryConditions.Add(outcome);
+            return outcome;
+        }
+        static class Program
+    {
+
+        }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
